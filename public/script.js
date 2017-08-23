54d83961fdd228235584b9bb94e06add17b78296
smithrm941 <smithrm941@gmail.com>
2017-08-23 14:59:53 -0700

    multiple button presses don't append duplicate data to ul anymore

--- public/script.js
+++ public/script.js
@@ -3,6 +3,7 @@ console.log("Sanity Check: JS is working!");
 $(document).ready(function(){
   $('.get-pokemon').on('click', function() {
     $.getJSON('https://mutably.herokuapp.com/pokemon', function(data) {
+      $('.list-group').empty();
       $.each(data.pokemon, function(key, value){
         $('.list-group').append('<li> <img src='+value.image+'><br>'
         + '<b>Name:</b> '+value.name+ ''
