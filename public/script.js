ef7670a5ee33badf62d92f6125e4ad0890421359
smithrm941 <smithrm941@gmail.com>
2017-08-23 14:46:53 -0700

    tidied up some code and appearance of Pokemon info on page

--- public/script.js
+++ public/script.js
@@ -4,7 +4,10 @@ $(document).ready(function(){
   $('.get-pokemon').on('click', function() {
     $.getJSON('https://mutably.herokuapp.com/pokemon', function(data) {
       $.each(data.pokemon, function(key, value){
-        $('.list-group').append('<li> <img src='+value.image+'><br>Name: '+value.name+' Pokédex No.: '+value.pokedex+' Evolves From:'+value.evolves_from+'</li>')
+        $('.list-group').append('<li> <img src='+value.image+'><br>'
+        + '<b>Name:</b> '+value.name+ ''
+        + ' <b>Pokédex No:</b> '+value.pokedex+ ''
+        + ' <b>Evolves From:</b> '+value.evolves_from+ '</li><br>')
       });
     });
   });
