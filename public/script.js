799cd723c4175b9ff10a95f8e9b8dd497460003b
smithrm941 <smithrm941@gmail.com>
2017-08-23 11:51:26 -0700

    button makes ajax request for pokemon data, nothing on page yet

--- public/script.js
+++ public/script.js
@@ -1,7 +1,10 @@
 console.log("Sanity Check: JS is working!");
 
 $(document).ready(function(){
+  $('.get-pokemon').on('click', function() {
+    $.getJSON('https://mutably.herokuapp.com/pokemon', function(data) {
+      console.log(data);
 
-  // code in here
-
+    });
+  });
 });
