601b38c11482a3ec10a53043ddc10b37787083a7
smithrm941 <smithrm941@gmail.com>
2017-08-23 15:20:45 -0700

    each pokemon has edit button with event delegation

--- public/script.js
+++ public/script.js
@@ -5,11 +5,16 @@ $(document).ready(function(){
     $.getJSON('https://mutably.herokuapp.com/pokemon', function(data) {
       $('.list-group').empty();
       $.each(data.pokemon, function(key, value){
-        $('.list-group').append('<li> <img src='+value.image+'><br>'
+        $('.list-group').append('<button>EDIT</button><br>'
+        + '<li> <img src='+value.image+'><br>'
         + '<b>Name:</b> '+value.name+ ''
         + ' <b>Pokédex No:</b> '+value.pokedex+ ''
         + ' <b>Evolves From:</b> '+value.evolves_from+ '</li><br>')
       });
+      $('.list-group button').addClass('edit-button');
     });
+  });
+  $('.list-group').on('click','.edit-button', function() {
+    console.log('please clap');
   });
 });
