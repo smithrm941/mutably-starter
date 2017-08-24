b729d10a0d4f7c5389e153b021e9342db62d5b0c
smithrm941 <smithrm941@gmail.com>
2017-08-23 17:11:59 -0700

    removed unneccessary addClass functions, added classes to pokemon data

--- public/script.js
+++ public/script.js
@@ -5,12 +5,11 @@ $(document).ready(function(){
     $.getJSON('https://mutably.herokuapp.com/pokemon', function(data) {
       $('.list-group').empty();
       $.each(data.pokemon, function(key, value){
-        $('.list-group').append('<button>EDIT</button><br>'
-        + '<li> <img src='+value.image+'><br>'
-        + '<b>Name:</b> '+value.name+ ''
-        + ' <b>Pokédex No:</b> '+value.pokedex+ ''
-        + ' <b>Evolves From:</b> '+value.evolves_from+ '</li><br>')
-        $('.list-group button').addClass('edit-button');
+        $('.list-group').append('<button class="edit-button">EDIT</button><br>'
+        + '<li> <img class="pokemon-image" src='+value.image+'><br>'
+        + '<b>Name:</b> <p class="pokemon-name">'+value.name+ '</p>'
+        + ' <b>Pokédex No:</b> <p class="pokedex-number">'+value.pokedex+ '</p>'
+        + ' <b>Evolves From:</b> <p class="evolves-from">'+value.evolves_from+ '</p></li><br>')
       });
     });
   });
